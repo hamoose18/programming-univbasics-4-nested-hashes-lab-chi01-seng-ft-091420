bb4d313793ae81e653ef5d347352039f47b18b94
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-09 17:47:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,10 +15,29 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-    :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
+# }
+# return the entire updated hash
+
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+
 end
 
 def changing_alan
